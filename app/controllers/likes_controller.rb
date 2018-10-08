54d1985a122d980763a7226ec00d1b282c3d13ec
gawa <yojimax822@gmail.com>
2018-10-08 13:08:10 +0000

    like ver2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -13,10 +13,10 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:micropost_id])
-    current_user.unlike(user)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unlike(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to  user
+    redirect_to  current_user
   end
   
   
